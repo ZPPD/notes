4cf1ab6bc3f6670a8722d266a119acf60d951cd2
ZPPD <zhana.p.d@gmail.com>
2019-03-23 09:27:16 -0400

    Updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Define UI var
-const form = document.querySelector('#task-form');
-const notes = document.querySelector('.list-group');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const noteInput = document.querySelector('#task');
+const form = document.querySelector("#task-form");
+const notes = document.querySelector(".list-group");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const noteInput = document.querySelector("#task");
 
 //loading event listeners, calling the function
 loadEventListeners();
@@ -11,37 +11,37 @@ loadEventListeners();
 //load all listeners
 function loadEventListeners() {
   //DOM event for Local Storage
-  document.addEventListener('DOMContentLoaded', getNotes);
+  document.addEventListener("DOMContentLoaded", getNotes);
 
   //add note event
-  form.addEventListener('submit', addNotes);
+  form.addEventListener("submit", addNotes);
   //remove note event
-  notes.addEventListener('click', removeNotes);
+  notes.addEventListener("click", removeNotes);
   //clear all notes
-  clearBtn.addEventListener('click', clearNotes);
+  clearBtn.addEventListener("click", clearNotes);
   //filter through notes
-  filter.addEventListener('keyup', filterNotes);
+  filter.addEventListener("keyup", filterNotes);
 }
 
 //get notes from LS
 function getNotes() {
-  let note;
-  if (localStorage.getItem('note') === null) {
-    note = [];
+  let noteList;
+  if (localStorage.getItem("noteList") === null) {
+    noteList = [];
   } else {
-    note = JSON.parse(localStorage.getItem('note'));
+    noteList = JSON.parse(localStorage.getItem("noteList"));
   }
-  note.forEach(function(nots) {
+  noteList.forEach(function(nots) {
     //create li el
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     //add class
-    li.className = 'list-group-item';
+    li.className = "list-group-item";
     //create a text node and append to it
     li.appendChild(document.createTextNode(nots));
     //create new link el
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     //add class
-    link.className = 'delete-item float-right text-danger';
+    link.className = "delete-item float-right text-danger";
     //add icon
     link.innerHTML = '<i class="fa fa-remove"></i>';
     //append the link to the li
@@ -53,20 +53,20 @@ function getNotes() {
 
 //add Notes
 function addNotes(e) {
-  if (noteInput.value === '') {
-    alert('Add a Note');
+  if (noteInput.value === "") {
+    alert("Add a Note");
   } else {
     //create li element
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     //console.log(li);
     //add class
-    li.className = 'list-group-item';
+    li.className = "list-group-item";
     //create text node and append to it
     li.appendChild(document.createTextNode(noteInput.value));
     //create new link el for removing
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     //add class
-    link.className = 'delete-item float-right text-danger';
+    link.className = "delete-item float-right text-danger";
     //add icon
     link.innerHTML = '<i class="fa fa-remove"></i>';
     //append the link to the li
@@ -78,28 +78,28 @@ function addNotes(e) {
     storeNoteInLocalStorage(noteInput.value);
 
     //clear input
-    noteInput.value = '';
+    noteInput.value = "";
     e.preventDefault();
   }
 }
 
 //store Note
 function storeNoteInLocalStorage(note) {
-  let notes;
-  if (localStorage.getItem('notes') === null) {
-    notes = [];
+  let noteList;
+  if (localStorage.getItem("noteList") === null) {
+    noteList = [];
   } else {
-    notes = JSON.parse(localStorage.getItem('notes'));
+    noteList = JSON.parse(localStorage.getItem("noteList"));
   }
-  notes.push(note);
+  noteList.push(note);
 
-  localStorage.setItem('notes', JSON.stringify(notes));
+  localStorage.setItem("noteList", JSON.stringify(noteList));
 }
 
 //remove note
 function removeNotes(e) {
-  if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are you sure?')) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are you sure?")) {
       e.target.parentElement.parentElement.remove();
 
       //remove from LS
@@ -110,24 +110,24 @@ function removeNotes(e) {
 
 //remove from LS
 function removeNoteFromLocalStorage(noteItem) {
-  let notes;
-  if (localStorage.getItem('notes') === null) {
-    notes = [];
+  let noteList;
+  if (localStorage.getItem("noteList") === null) {
+    noteList = [];
   } else {
-    notes = JSON.parse(localStorage.getItem('notes'));
+    noteList = JSON.parse(localStorage.getItem("noteList"));
   }
-  notes.forEach(function(note, index) {
+  noteList.forEach(function(note, index) {
     if (noteItem.textContent === note) {
-      notes.splice(index, 1);
+      noteList.splice(index, 1);
     }
   });
-  localStorage.setItem('notes', JSON.stringify(notes));
+  localStorage.setItem("noteList", JSON.stringify(noteList));
 }
 
 //clear all notes
 function clearNotes() {
   while (notes.firstChild) {
-    if (confirm('You will loose all notes')) {
+    if (confirm("You will loose all notes")) {
       notes.removeChild(notes.firstChild);
     }
   }
@@ -144,14 +144,13 @@ function filterNotes(e) {
   //to get what we type
   const text = e.target.value.toLowerCase();
   //loop through the list, queryselall returns a node list, so we can use foreach
-  document.querySelectorAll('.list-group-item').forEach(
-    function(task) {
-      const item = task.firstChild.textContent;
+  document.querySelectorAll(".list-group-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
 
-      if (item.toLowerCase().indexOf(text) != -1) {
-        task.style.display = 'block';
-      } else {
-        task.style.display = 'none';
-      }
-    });
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
